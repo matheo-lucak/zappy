8c730826469a6509421ccd06572d60e0ea8ebc3e
Guillaume Bogard <guillaumebgd@hotmail.fr>
2021-05-31 19:06:41 +0200

    [ADD] Added mylist library in server's includes

---
+++ server/libs/mylist/my_delete_node.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** My_Lib_C
+** File description:
+** my_delete_node.c
+*/
+
+#include <stdlib.h>
+#include "mylist.h"
+
+void my_delete_node(list_t *list, node_t *node, void (*free_function)())
+{
+    if (!my_remove_node(list, node))
+        return;
+    if (free_function != NULL && NODE_DATA(node, void *) != NULL)
+        free_function(NODE_DATA(node, void *));
+    free(node->data.ptr);
+    free(node);
+}
