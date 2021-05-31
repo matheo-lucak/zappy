31108c1e54229b346f75b1fd9f9ecb4881d1d5b6
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-05-31 17:38:05 +0200

    [ADD] Added argument parsing
    Co-authored-by: Ligny <Ligny@users.noreply.github.com>

---
+++ ai/zappy_ai.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: Utf-8 -*
+
+from typing import NamedTuple
+
+class ZappyAI(NamedTuple):
+    machine: str
+    port: int
+    name: str
+
+def zappy_ai(args: ZappyAI) -> None:
+    pass
