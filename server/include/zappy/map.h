b1e5dbb65c9a223425bfe17fa0b0c79dd2c310ab
Guillaume Bogard <guillaumebgd@hotmail.fr>
2021-06-08 14:30:24 +0200

    [ADD] Zappy Headers

---
+++ server/include/zappy/map.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** Zappy [WSL: Ubuntu]
+** File description:
+** map
+*/
+
+#ifndef MAP_H_
+#define MAP_H_
+
+typedef struct map_s
+{
+    unsigned int width;
+    unsigned int height;
+} map_t;
+
+#endif /* !MAP_H_ */
---
+++ server/include/zappy/teams.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** Zappy [WSL: Ubuntu]
+** File description:
+** teams
+*/
+
+#ifndef TEAMS_H_
+#define TEAMS_H_
+
+typedef struct team_s
+{
+    char *name;
+    unsigned population_size;
+} team_t;
+
+#endif /* !TEAMS_H_ */
