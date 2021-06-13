3231d4f6c01e89ad92967f82ce5bdf2784203829
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-06-13 15:48:21 +0200

    [UPDATE] Removed uniform delete of nodes -> Node destructions are now specific to the list

--- server/libs/mylist/mylist/container.h
+++ server/libs/mylist/mylist/container.h
@@ -25,14 +25,6 @@ int container_add_node_at_end(container_list_t *list, node_t *element);
 
 int container_add_node(container_list_t *list, node_t *node, long index);
 
-void container_clear(container_list_t *list);
-
-void container_delete_first_node(container_list_t *list);
-
-void container_delete_last_node(container_list_t *list);
-
-void container_delete_node(container_list_t *list, long index);
-
 node_t *container_get_node(const container_list_t *list, long index);
 
 void container_merge(container_list_t *list1, container_list_t *list2);
@@ -56,8 +48,6 @@ node_t *create_node(const void *data, size_t size);
 node_t *create_ptr_node(void *ptr);
 
 node_t *create_string_node(const char *str);
-
-void destroy_node(node_t *node, node_dtor_t destructor);
 /////////////////////////////////////////////////////////////////////////////
 
 #endif /* !MY_LIST_CONTAINER_H_ */
