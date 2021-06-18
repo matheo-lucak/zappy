fc9d80eb96e59aae4e5bdfdc4c4883793982efb1
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-06-18 10:28:40 +0200

    [FIX] Removed useless memcmp()

--- server/libs/mylist/ptr_list/ptr_list_find.c
+++ server/libs/mylist/ptr_list/ptr_list_find.c
@@ -13,7 +13,7 @@ const node_t *intern_ptr_list_find(const ptr_list_t *this, const void *ptr)
     if (!ptr)
         return NULL;
     list_foreach(node, this) {
-        if (memcmp(&(node->data.ptr), &ptr, sizeof(ptr)) == 0)
+        if (node->data.ptr == ptr)
             return node;
     }
     return NULL;
