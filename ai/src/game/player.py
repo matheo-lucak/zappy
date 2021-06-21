55776472e87c285d4a2c5d3da596b4d6436a0f77
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-06-21 18:11:30 +0200

    [UPDATE] Received message are buffered

--- ai/src/game/player.py
+++ ai/src/game/player.py
@@ -1,11 +1,25 @@
 # -*- coding: Utf-8 -*
 
+from typing import List
+
 from .inventory import Inventory
 from .vision import Vision
 from ..api_server import APIServer
 from ..api_server.request import BroadcastRequest, InventoryRequest, LookRequest
 from ..api_server.request.response.spontaneous import MessageResponse
 
+class Message:
+    def __init__(self, tile: int, text: str) -> None:
+        self.__tile: int = tile
+        self.__text: str = text
+
+    @property
+    def tile(self) -> int:
+        return self.__tile
+
+    @property
+    def text(self) -> str:
+        return self.__text
 
 class Player:
     def __init__(self, team_name: str, api: APIServer) -> None:
@@ -14,6 +28,7 @@ class Player:
         self.__vision: Vision = Vision()
         self.__alive: bool = True
         self.__api: APIServer = api
+        self.__messages: List[Message] = list()
 
     def update(self) -> None:
         if not self.__api.has_request_to_handle(InventoryRequest):
@@ -31,10 +46,16 @@ class Player:
 
     def listen(self, message: MessageResponse) -> None:
         print(f"From tile {message.tile}: {repr(message.text)}")
+        self.__messages.append(Message(message.tile, message.text))
 
     def broadcast(self, message: str) -> None:
         print(f"Broadcasting: {repr(message)}")
         self.__api.send(BroadcastRequest(message))
+
+    def flush_messages(self) -> List[Message]:
+        messages: List[Message] = self.__messages
+        self.__messages = list()
+        return messages
 
     @property
     def team(self) -> str:
