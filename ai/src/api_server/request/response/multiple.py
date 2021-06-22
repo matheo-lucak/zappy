c42f96a730cc4b05a4abd0fb6c8cf88882a47e5f
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-06-22 16:52:05 +0200

    [ADD] MultiResponse class model

---
+++ ai/src/api_server/request/response/multiple.py
@@ -0,0 +1,34 @@
+# -*- coding: Utf-8 -*
+
+from typing import Any
+
+from .base import Response
+
+
+class MultiResponse(Response):
+    def __init_subclass__(cls, /, *, nb_responses: int, wait_for_all: bool) -> None:
+        super().__init_subclass__()
+        if nb_responses < 1:
+            raise ValueError("nb_responses must be a positive integer")
+        setattr(cls, "__nb_responses__", nb_responses)
+        setattr(cls, "__wait_all__", wait_for_all)
+
+    def __new__(cls, response: str, *other_responses: str, **kwargs: Any) -> Any:
+        if cls is MultiResponse:
+            raise TypeError(f"{cls.__name__} can't be instantiated")
+        return super().__new__(cls)
+
+    def __init__(self, *responses: str) -> None:
+        super().__init__(self.END_RESPONSE.join(responses))
+
+    @classmethod
+    def nb_responses(cls) -> int:
+        if cls is MultiResponse:
+            raise AttributeError(f"Base class {cls.__name__} does not have this attribute")
+        return getattr(cls, "__nb_responses__")
+
+    @classmethod
+    def wait_for_all(cls) -> bool:
+        if cls is MultiResponse:
+            raise AttributeError(f"Base class {cls.__name__} does not have this attribute")
+        return getattr(cls, "__wait_all__")
