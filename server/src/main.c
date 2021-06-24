794e90493d9df5eae795cf64942f4cfb8ddd3c7e
Guillaume Bogard <59202002+guillaumebgd@users.noreply.github.com>
2021-06-24 15:02:27 +0200

    Update main.c

--- server/src/main.c
+++ server/src/main.c
@@ -13,13 +13,6 @@
 
 int main(int ac, char **av)
 {
-    response_t *res1 = response_create(RESPONSE_WELCOME);
-    response_t *res2 = response_create(RESPONSE_CLIENT_NUM, 5);
-    response_t *res3 = response_create(RESPONSE_XY, 15, 29);
-    response_t *res4 = response_create(RESPONSE_OK);
-    response_t *res5 = response_create(RESPONSE_KO);
-    response_t *res6 = response_create(RESPONSE_DEAD);
-
     arguments_t args = arguments_default_values();
 
     if (parse_arguments(ac, av, &args))
