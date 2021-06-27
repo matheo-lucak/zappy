b32ba4c753c05de1dae515bd42894000fd1a4daa
Guillaume Bogard <59202002+guillaumebgd@users.noreply.github.com>
2021-06-27 19:40:38 +0200

    Update main.c

--- server/src/main.c
+++ server/src/main.c
@@ -10,19 +10,6 @@
 
 int main(int ac, char **av)
 {
-    server_t s = {0};
-    team_t *t1 = NULL;
-    team_t *t2 = NULL;
-
-    set_dummy_server(&s, 2, 2);
-    t1 = NODE_PTR(list_get(s.sim.teams, 0), team_t);
-    t2 = NODE_PTR(list_get(s.sim.teams, 1), team_t);
-    server_remove_client(&s, NODE_PTR(list_get(s.clients, 0), client_t));
-    server_remove_client(&s, NODE_PTR(list_get(s.clients, 0), client_t));
-    server_remove_client(&s, NODE_PTR(list_get(s.clients, 0), client_t));
-    server_remove_client(&s, NODE_PTR(list_get(s.clients, 0), client_t));
-    server_stop(&s);
-
     arguments_t args = arguments_default_values();
 
     if (parse_arguments(ac, av, &args))
